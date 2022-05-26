7d6eb023a0991edf02839891122ca31e8938ad7d
AryanPatel200 <aryan.jnext@gmail.com>
2022-05-26 12:24:22 +0530

    script for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('App listenong on port: ', port)
